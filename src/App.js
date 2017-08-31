ea668d692338bfbb4159f896203d94af55549a28
Ivan Migov <ivan@revimedia.com>
2017-08-31 15:25:08 +0300

    add bootstrap less + waching css changes

--- src/App.js
+++ src/App.js
@@ -8,11 +8,17 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2 className="text-info">Welcome to React</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-6">1</div>
+            <div className="col-sm-6">2</div>
+          </div>
+        </div>
       </div>
     );
   }
