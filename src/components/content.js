d7646d2b2228a179ed852c3db0334a34ec6a73f2
Ivan Migov <ivan@revimedia.com>
2017-09-12 19:57:29 +0300

    show header and list via router

--- src/components/content.js
+++ src/components/content.js
@@ -2,24 +2,31 @@ import React from 'react';
 import { Component } from 'react';
 import LeftSideBar   from './left-side-bar';
 import ToDoList   from './todo_list';
-import EditToDoForm   from './edit-todo';
 import Filters   from './todo-filters';
 
 
 
-export default class Content extends Component {
-  render() {
-    return (
-      <div className="td-content" >
-        <LeftSideBar/>
-        <div className="td-content-main">
-          <Filters/>
-          <div className="td-content-list">
-            <ToDoList/>
-            <EditToDoForm/>
+export default function(ComposedComponent){
+  class Content extends Component {
+    getChildComponent(){
+      return ComposedComponent ? <ComposedComponent/> : '';
+    }
+    render() {
+      console.log('ComposedComponent',ComposedComponent);
+      return (
+        <div className="td-content" >
+          <LeftSideBar/>
+          <div className="td-content-main">
+            <Filters/>
+            <div className="td-content-list">
+              <ToDoList/>
+              <div className="td-test"></div>
+              {this.getChildComponent()}
+            </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
+  return Content;
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import './index.css';
 import './css/main.css';
-import App from './App';
+import './App.css';
+import Content from './components/content';
+import Header from './components/header';
+import EditToDoForm   from './components/edit-todo';
+
 import reducers from './reducers';
 import Async from './middlewares/async';
 
@@ -15,7 +20,15 @@ const createStoreWithMiddleware = applyMiddleware(Async)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+          <Header/>
+          <Switch>
+            <Route path="/todos/:id" component={Content(EditToDoForm)}/>
+            <Route path="/todos" component={Content()}/>
+          </Switch>
+      </div>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root'));
 registerServiceWorker();
